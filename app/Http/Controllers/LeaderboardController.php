264e1f4879e187c67ecf362ff1042a136f6cf95b
Carlos Resultay <resultaycarlos@gmail.com>
2026-01-20 10:16:10 +0800

    Leaderboard API update

--- app/Http/Controllers/LeaderboardController.php
+++ app/Http/Controllers/LeaderboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\LeaderboardCollection;
+use App\Http\Resources\LeaderboardEntryResource;
 use App\Services\API\v1\Leaderboard\LeaderboardService;
 
 class LeaderboardController extends Controller
@@ -13,6 +14,10 @@ class LeaderboardController extends Controller
 
     public function index()
     {
-        return new LeaderboardCollection($this->leaderboardService->getLeaderboard());
+        return new LeaderboardCollection(
+            LeaderboardEntryResource::collection(
+                $this->leaderboardService->getLeaderboard()
+            )
+        );
     }
 }
